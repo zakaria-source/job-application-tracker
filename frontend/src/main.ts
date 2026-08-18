29e13910afd1048acff95ebbe430ff439e3edcf4
Zakaria Dbaba <111566321+zakaria-source@users.noreply.github.com>
2026-08-18 15:59:39 +0200

    refactor: bootstrap from core auth structure

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,15 +3,15 @@ import {provideZoneChangeDetection} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideRouter} from '@angular/router';
 import {provideCharts, withDefaultRegisterables} from 'ng2-charts';
+import {authInterceptor} from '@app/core/auth/auth.interceptor';
 import {App} from './app/app.component';
 import {APP_ROUTES} from './app/app.routes';
-import {cloudAuthInterceptor} from './app/cloud/auth.interceptor';
 
 bootstrapApplication(App, {
-    providers: [
-        provideCharts(withDefaultRegisterables()),
-        provideZoneChangeDetection(),
-        provideHttpClient(withInterceptors([cloudAuthInterceptor])),
-        provideRouter(APP_ROUTES)
-    ]
+  providers: [
+    provideCharts(withDefaultRegisterables()),
+    provideZoneChangeDetection(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(APP_ROUTES)
+  ]
 }).catch(err => console.error(err));
